f7532e28a0225d66c02bf5a3f873dc6ded07d426
Edison Gonzalez <javierg@nserio.com>
2017-03-30 17:22:24 -0500

    Set up SignalR and Startup

---
+++ RelativityInspector/RelativityInspector.Web/Controllers/AuditController.cs
@@ -0,0 +1,26 @@
+﻿using RelativityInspector.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace RelativityInspector.Web.Controllers
+{
+    public class AuditController : ApiController
+    {
+        public InspectResult Inspect(IEnumerable<int> objectTypes)
+        {
+            var result = new InspectResult();
+            try
+            {
+                result.Success = true;
+            }
+            catch (Exception e)
+            {
+                result.Message = e.Message;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ RelativityInspector/RelativityInspector.Web/Models/AuditItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RelativityInspector.Web.Models
+{
+    public class AuditItem
+    {
+        public int AuditID { get; internal set; }
+        public DateTime LastExecutionDate { get; internal set; }
+        public string Name { get; internal set; }
+        public string Status { get; internal set; }
+    }
+}
