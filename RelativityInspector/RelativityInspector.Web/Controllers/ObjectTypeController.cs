405fa42101c6fda8f1713f954d0172eb5395ad74
Edison Gonzalez <javierg@nserio.com>
2017-03-30 20:58:01 -0500

    WebApi List of Object Types

---
+++ RelativityInspector/RelativityInspector.Web/Controllers/ObjectTypeController.cs
@@ -0,0 +1,17 @@
+﻿using RelativityInspector.Web.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RelativityInspector.Web.Controllers
+{
+    public class ObjectTypeController: ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Artifact> ObjectTypes()
+        {
+            var repository = new AuditRepository();
+            var listOfObjectTypes = repository.ObjectTypes();
+            return listOfObjectTypes;
+        }
+    }
+}
